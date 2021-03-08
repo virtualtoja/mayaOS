52be494eeb01a1cce8fe489d491e30feb5cc745d
virtualtoja <76951241+virtualtoja@users.noreply.github.com>
2021-03-08 20:16:26 +0100

    Public Build 1
    The First public build

---
+++ mayaOS/AppHelper/Taskmanager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace mayaOS.AppHelper
+{
+    public static class Taskmanager
+    {
+        public static void EndProcess(int pid)
+        {
+            Process.Terminate(true, pid);
+        }
+
+        public static void StartProcess()
+        {
+
+        }
+    }
+}
